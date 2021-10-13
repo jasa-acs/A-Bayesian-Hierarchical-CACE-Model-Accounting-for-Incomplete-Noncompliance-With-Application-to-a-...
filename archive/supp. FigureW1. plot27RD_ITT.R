4d86202dac2ffd6d301cf662c14df5bdfc3d2f71
JASA Applications & Case Studies <JASA.app.cs.aer@gmail.com>
2021-10-13 16:46:44 -0500

    Add files via upload

---
+++ archive/supp. FigureW1. plot27RD_ITT.R
@@ -0,0 +1,82 @@
+library(forestplot)
+library(metafor)
+setwd("submitted")
+data <- read.table("data/data.txt", sep="", header = TRUE)
+data$author_year <- paste(data$Author, data$Year, sep=", ")
+data$author_year[1:10] <- paste(data$author_year[1:10], " *", sep="")
+
+data0 <- read.table("output/CACEi.txt", sep="", header = TRUE)
+data1 <- cbind(data$author_year, data0)
+colnames(data1)<- c("Author_year", "Lower", "Mean", "Median", "Upper", "SD", "HPD_l", "HPD_u")
+data2 <- data1[order(data1$Author_year), ] 
+
+result <- read.table("output/out2_5c_cacei.txt", sep="", header = TRUE)[c("CACE"), c("X2.5.", "X50.", "X97.5.")]
+
+data_ITT <- read.table("data/data_ITT.txt", sep="", header = TRUE)
+data_test <- data_ITT[data_ITT$miss==1, ]
+
+ma_m <- rma(ai=n1s1, bi=n1s0, ci=n0s1, di=n0s0, data = data_ITT, measure = "RD", 
+            method="FE", slab=Author_Year, digits=3)
+# built in forest plot function:
+forest(ma_m, header=TRUE, digits=3)
+
+ma_m$yi
+ci.lb <- ma_m$yi - qnorm(0.025, lower.tail=FALSE) * sqrt(ma_m$vi)
+ci.ub <- ma_m$yi + qnorm(0.025, lower.tail=FALSE) * sqrt(ma_m$vi)
+
+# # add analysis for a single study, for example, Ramin 1995
+# data_R1995 <- subset(data_ITT, data_ITT$Author_Year=="Clark_1998")
+# ma_R1995 <- rma(ai=n1s1, bi=n1s0, ci=n0s1, di=n0s0, data = data_R1995, measure = "RD", 
+#             method="FE", slab=study)
+# ma_R1995
+
+
+meandata <- ma_m$yi[1:27]
+mediandata <- ma_m$yi[1:27]
+lowerdata <- ci.lb[1:27]
+upperdata <- ci.ub[1:27]
+
+RD_for_each <- 
+  structure(list(
+    mean  = c(NA, NA, meandata, round(as.numeric(ma_m$b), 3)), 
+    lower = c(NA, NA, lowerdata, round(as.numeric(ma_m$ci.lb), 3) ), 
+    upper = c(NA, NA, upperdata, round(as.numeric(ma_m$ci.ub), 3) )),
+    .Names = c("mean", "lower", "upper"), 
+    row.names = c(NA, -30L), 
+    class = "data.frame")
+
+tabletext<-cbind(
+  c("", "Study (Author, Year)", paste(data2$Author_year), "Overall RD (ITT)"),
+  
+  c("", "RD", paste(formatC(round(meandata, 3), format='f', digits=3 ), " (",
+                      formatC(round(lowerdata, 3), format='f', digits=3 ),", ", 
+                      formatC(round(upperdata, 3), format='f', digits=3 ), ")", sep="") , 
+    paste(round(ma_m$b, 3), " (", 
+        round(ma_m$ci.lb, 3), ", ", 
+        round(ma_m$ci.ub, 3), ")", sep="" ) )
+)
+
+own.f <- fpTxtGp(ticks = gpar(cex=0.85), xlab  = gpar(cex = 0.85))
+
+xticks <- seq(from = -.3, to = 0.45, by = 0.1)
+xtlab <- rep(c(TRUE, FALSE), length.out = length(xticks))
+attr(xticks, "labels") <- xtlab
+
+# CACE_complete <- CACE_for_each
+# CACE_marginal <- CACE_for_each
+# CACE_marginal[c(3, 4, 10, 11, 14, 20, 21, 23, 24, 29),]<- NA
+# CACE_complete[-c(3, 4, 10, 11, 14, 20, 21, 23, 24, 29),]<- NA
+
+forestplot(tabletext, 
+           boxsize = .2, 
+           hrzl_lines = gpar(lwd=1, col="#444444"),
+           txt_gp = own.f,
+           mean = RD_for_each[, "mean"],
+           lower = RD_for_each[, "lower"],
+           upper = RD_for_each[, "upper"],
+           new_page = TRUE, 
+           is.summary=c(TRUE, TRUE, rep(FALSE,27), TRUE, TRUE),
+           clip=c(-1.0, 1.0), 
+           # lty.ci = c(1, 5),
+           col=fpColors(box="darkred",line="darkred", summary="darkred"), 
+           xticks = xticks) 
